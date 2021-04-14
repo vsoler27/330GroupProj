da841a6a13e121d0fdc499a7d814cf51078b2955
vsoler27 <60946932+vsoler27@users.noreply.github.com>
2021-04-14 17:06:35 -0400

    Add files via upload

---
+++ DataLayer.java
@@ -0,0 +1,145 @@
+/**
+ * Group Project 
+ * Data Layer program 
+ * 04/12/21
+ */
+import java.sql.*;
+import javax.swing.*;
+
+public class DataLayer {
+   /* Methods needed:
+      Connect method - do we want a pop up?
+      faculty update
+         works
+         interests
+      faculty delete
+         works
+         interests
+      public view 
+         works
+         interests
+      public search
+         works
+         interests
+      student view 
+         works
+         interests
+      student search 
+         works
+         interests
+   */
+   private Connection conn;
+   private ResultSet rs;
+   private ResultSetMetaData rsmd;
+   private Statement stmt;
+   private String sql;
+   
+   final String DEFAULT_DRIVER = "com.mysql.cj.jdbc.Driver";
+   
+   public boolean connect(){
+      conn = null;
+      String userName = "root";
+      String password = "student";
+      String url = "jdbc:mysql://localhost/project";
+      url = url + "?serverTimezone=UTC";
+
+	   String passwordInput = JOptionPane.showInputDialog(null,"Enter password: ",
+	                     "Question", JOptionPane.QUESTION_MESSAGE);
+      
+      password = passwordInput;
+      
+      try{
+         Class.forName(DEFAULT_DRIVER);
+         conn = DriverManager.getConnection(url, userName, password);
+      }
+      catch(ClassNotFoundException cnfe){
+		 System.out.println("No DB Connection");
+         System.out.println("Class");
+         System.out.println("ERROR MESSAGE-> "+cnfe);
+         System.exit(0);
+      }
+      catch(SQLException sqle){
+		 System.out.println("ERROR SQLExcepiton in connect()");
+		 System.out.println("ERROR MESSAGE -> "+sqle);
+         sqle.printStackTrace();
+         System.exit(0);
+      }//end of catch
+
+      return (conn!=null);
+   } //end of connect
+   
+   public boolean close(){
+      try {
+         if (!conn.isClosed()) {
+            rs.close();
+            stmt.close();
+            conn.close();
+            return true;
+         }//end if
+         else {
+            return false; 
+         }//end else
+      }
+      catch(SQLException sqle){
+        System.out.println("ERROR IN METHOD close()");
+        System.out.println("ERROR MESSAGE -> "+sqle);
+        return false;
+      }// end of catch
+   }//end of method close
+
+   /**
+    * Inserts an interest for faculty 
+    * @param userID the user adding the interest 
+    * @param interest the interest to be added
+    */
+   public void facultyInsertInterests (String userID, String interest) {
+      try {
+         PreparedStatement stmt = conn.prepareStatement("INSERT INTO user_interests(userID,interest) VALUES(?,?) ");
+         stmt.setString(1, userID);
+         stmt.setString(2, interest);
+         stmt.executeUpdate();
+      }//end try
+      catch (SQLException sqle) {
+         System.out.println("ERROR IN METHOD facultyInsertInterests()");
+        System.out.println("ERROR MESSAGE -> "+sqle);
+      }//end catch
+   }//end facultyInsertInterests
+
+   /**
+    * Inserts a work for faculty
+    * @param userID the user adding the work
+    * @param desc the abstract of the work
+    * @param date the date of the work
+    */
+   public void facultyInsertWork(String userID, String desc, String date) {
+      try {
+         PreparedStatement stmt = conn.prepareStatement("INSERT INTO works(userID,abstract,date) VALUES (?,?,?)");
+         stmt.setString(1, userID);
+         stmt.setString(2, desc);
+         stmt.setString(3, date);
+         stmt.executeUpdate();
+      } catch (SQLException sqle) {
+         System.out.println("ERROR IN METHOD facultyInsertWork()");
+         System.out.println("ERROR MESSAGE -> "+sqle);
+      }
+   }//end facultyInsertWork
+
+   /**
+    * Updates a users interest
+    * @param userID the user
+    * @param interest the new interest
+    */
+   public void facultyUpdateInterests (String userID, String interest) {
+      try {
+         PreparedStatement stmt = conn.prepareStatement("UPDATE user_interest SET interest=? WHERE userID=?");
+         stmt.setString(1, interest);
+         stmt.setString(2, userID);
+         stmt.executeUpdate();
+      } catch (SQLException sqle) {
+         System.out.println("ERROR IN METHOD facultyUpdateInterest()");
+         System.out.println("ERROR MESSAGE -> "+sqle);
+      }
+   }
+
+
+}//end class
---
+++ project.sql
@@ -0,0 +1,48 @@
+DROP DATABASE IF EXISTS project;
+CREATE DATABASE project;
+use project; 
+
+DROP TABLE IF EXISTS user;
+CREATE TABLE user (
+userId INT AUTO_INCREMENT, -- PK
+	role CHAR(1) -- (faculty, student, public),
+	firstName VARCHAR(25),
+	lastName VARCHAR(25),
+	Password VARCHAR(30),
+	Email_or_username VARCHAR(30),
+PRIMARY KEY (userId)
+);
+
+DROP TABLE IF EXISTS contact; 
+CREATE TABLE contact (
+	userId INT, -- FK
+	phoneNumber VARCHAR(10),
+	email VARCHAR (50) NOT NULL,
+	CONSTRAINT userIdcontact FOREIGN KEY (userId) REFERENCES user(userId) ON DELETE CASCADE
+);
+
+
+DROP TABLE IF EXISTS interests;
+CREATE TABLE interests (
+	interestId INT AUTO_INCREMENT -- PK,
+	interests TEXT,
+	PRIMARY KEY (interestId)
+);
+
+DROP TABLE IF EXISTS user_interests;
+CREATE TABLE user_interests (
+userId INT, -- FK
+interestId INT, -- FK
+CONSTRAINT user_interests FOREIGN KEY (userId) REFERENCES user(userId) ON DELETE CASCADE,
+CONSTRAINT user_interests FOREIGN KEY (interestId) REFERENCES interests(interestId) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS works;
+CREATE TABLE works (
+	worksId INT AUTO_INCREMENT -- PK,
+	userId INT, -- FK
+	Abstract TEXT,
+	Date DATE,
+	PRIMARY KEY (worksId),
+CONSTRAINT works FOREIGN KEY (userId) REFERENCES user(userId) ON DELETE CASCADE,
+);
