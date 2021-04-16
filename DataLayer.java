bcba53a2b6c83c50eaa8acaabd1710275595fae7
feilin7389 <78882445+feilin7389@users.noreply.github.com>
2021-04-15 22:58:46 -0400

    Update DataLayer.java

--- DataLayer.java
+++ DataLayer.java
@@ -190,5 +190,142 @@ public class DataLayer {
          System.out.println("ERROR MESSAGE -> "+sqle);
       }
    }
+	
+/**
+    * Register the user account and insert it to the database
+    * @param username the account username 
+    * @param password the account password
+    * @param fName the user's first name
+    * @param lName the user's last name
+    * @param role the role of the account
+    * @param phoneNumber the user's phone number
+    * @return 1 if account successfuly insert to database
+    * @return 0 if are not insert to database
+    */
+   public int register(String username, String password,String fName,String lName,String role,String phoneNumber) {
+      int result = 0;
+      
+      if (username.length() <= 30 && password.length() <= 30){
+         try {
+            stmt = conn.createStatement();
+            sql = "SELECT * FROM user Where Email_or_username ='" + username + "';";
+            rs = stmt.executeQuery(sql);
+            
+            if (!rs.next()){
+               try{
+                  PreparedStatement stmt1;
+               
+                  stmt1 = conn.prepareStatement("INSERT INTO user(Email_or_username,Password,firstName,lastName,role)  VALUES (?,?,?,?,?)",Statement.RETURN_GENERATED_KEYS);
+                  stmt1.setString(1,username);
+                  stmt1.setString(2,password);
+                  stmt1.setString(3,fName);
+                  stmt1.setString(4,lName);
+                  stmt1.setString(5,role);
+                  stmt1.executeUpdate();     // Performs the insert command
+               
+                  ResultSet rs1 = stmt1.getGeneratedKeys();
+                  rs1.next();
+                  int auto_id = rs1.getInt(1);
+                  System.out.println(auto_id);
+               
+                  try{
+                     PreparedStatement stmt2;
+                  
+                     stmt2 = conn.prepareStatement("INSERT INTO contact(phoneNumber,email,userid)  VALUES (?,?,?)");
+                     stmt2.setString(1,phoneNumber);
+                     stmt2.setString(2,username);
+                     stmt2.setInt(3,auto_id);
+                     result = stmt2.executeUpdate(); // Performs the insert command
+                  
+                  }catch(Exception e)
+                  {
+                     System.out.println("Error whlie trying to insert a contact record.");
+                     System.out.println("Error message is --> "+e);
+                  }//end of catch
+               
+               }// end of try
+               catch(Exception e)
+               {
+                  System.out.println("Error whlie trying to insert a user record or close.");
+                  System.out.println("Error message is --> "+e);
+               }//end of catch
+            
+            }
+            else {
+               System.out.print("This username are been use");
+            }
+         }// end of try
+         catch(Exception e)
+         {
+            System.out.println("Error whlie trying to select the data.");
+            System.out.println("Error message is --> "+e);
+         }//end of catch
+      }
+      else{
+         if (username.length() > 30){
+            System.out.println("Username too long!!!");
+         
+         }else{
+            System.out.println("Password too long!!!");
+         }
+      
+      }
+      
+      return (result);
+   }// end of method register
+   
+   /**
+    * Validate the user login and check to see their role
+    * @param username the account username 
+    * @param password the account password
+    */
+   public void login(String username, String password) {
+   
+      try {
+         stmt = conn.createStatement();
+         sql = "SELECT * FROM user Where Email_or_username ='" + username;
+         sql += "' AND Password = '" + password + "';";
+         rs = stmt.executeQuery(sql); 
+         if (rs.next()){ 
+           System.out.println("Successfuly login");
+           String role = rs.getString(2);
+           if (role.equals("s")|| role.equals("S")){
+               System.out.println("The role is Student");
+           }
+           else if (role.equals("f") || role.equals("F")){
+               System.out.println("The role is Faculty");
+           }
+           else if (role.equals("p") || role.equals("P")){
+               System.out.println("The role is public");
+           }                 
+         }
+         else{
+            System.out.println("username or password are incorrect");
+         }
+            
+      }// end of try
+      catch(Exception e)
+      {
+         System.out.println("Error whlie trying to select the data.");
+         System.out.println("Error message is --> "+e);
+      }//end of catch
+      
+   
+      
+   }// end of method login
+
+   
+
+   public static void main(String[] args) {
+      DataLayer db = new DataLayer();
+      if (db.connect()){
+         System.out.println("Database Connect!!!");
+         db.login("person@gmail.com","password");
+         
+      }// if connect 
+
+           
+      
+   }//end of main method
 
 }//end class
